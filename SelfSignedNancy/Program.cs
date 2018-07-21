885a5d8f8230c49ef9a332ecf65db8fbedbf44a2
Your Name <sadhikaria@gmail.com>
2018-07-21 17:33:54 -0400

    added files to git

---
+++ SelfSignedNancy/Program.cs
@@ -0,0 +1,42 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfSignedNancy
+{
+    class Program
+    {
+        private string _url = "https://localhost";
+        private int _port = 12348;
+        private NancyHost _nancy;
+
+        public Program()
+        {
+            var uri = new Uri($"{_url}:{_port}/");
+
+
+            //var configuration = new HostConfiguration()
+            //{
+            //    UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            //};
+            _nancy = new NancyHost(uri);
+        }
+
+        private void Start()
+        {
+            _nancy.Start();
+            Console.WriteLine($"Started listennig port {_port}");
+            Console.ReadKey();
+            _nancy.Stop();
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Start();
+        }
+    }
+}
---
+++ SelfSignedNancy/SimpleModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfSignedNancy
+{
+    public class SimpleModule : Nancy.NancyModule
+    {
+        public SimpleModule()
+        {
+            Get["/"] = _ => "Received GET request";
+
+            Post["/"] = _ => "Received POST request";
+
+            Get["/user/{id}"] = parameters =>
+            {
+                if (((int)parameters.id) == 666)
+                {
+                    return $"All hail user #{parameters.id}! \\m/";
+                }
+                else
+                {
+                    return "Just a regular user!";
+                }
+            };
+        }
+    }
+}
